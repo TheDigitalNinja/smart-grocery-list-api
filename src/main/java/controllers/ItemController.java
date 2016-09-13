fdf5fea9b7eba8d61259442f27c3b3999fd1b18e
Russell Perkins <dsw.ninjaboy@gmail.com>
2016-09-12 20:10:02 -0600

    Better api param handling

--- src/main/java/controllers/ItemController.java
+++ src/main/java/controllers/ItemController.java
@@ -1,9 +1,6 @@
 package controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
 @RestController
@@ -17,25 +14,28 @@ public class ItemController {
     }
 
     // Item C.R.U.D.
-    @RequestMapping(value = "/item", method = RequestMethod.GET)
-    public String itemResponce(@RequestParam(value="id", required=true) String id, Model model) {
+    @RequestMapping(value = "/item/{id}", method = RequestMethod.GET)
+    public String itemResponce(@PathVariable String id, Model model) {
         model.addAttribute("id", id);
-        return "Get item data controller";
+        return "Get item data controller. " + id.toString();
     }
 
-    @RequestMapping(value = "/item", method = RequestMethod.POST)
-    public String PostItemResponce(@RequestParam(value="id", required=true) String id, Model model) {
-        return "Post item controller";
+    @RequestMapping(value = "/item/{id}", method = RequestMethod.POST)
+    public String PostItemResponce(@PathVariable String id, Model model) {
+        model.addAttribute("id", id);
+        return "Post item controller. " + id.toString();
     }
 
-    @RequestMapping(value = "/item", method = RequestMethod.PUT)
-    public String PutItemResponce(@RequestParam(value="id", required=true) String id, Model model) {
-        return "Put item controller";
+    @RequestMapping(value = "/item/{id}", method = RequestMethod.PUT)
+    public String PutItemResponce(@PathVariable String id, Model model) {
+        model.addAttribute("id", id);
+        return "Put item controller. " + id.toString();
     }
 
-    @RequestMapping(value = "/item", method = RequestMethod.DELETE)
-    public String DeleteItemResponce(@RequestParam(value="id", required=true) String id, Model model) {
-        return "Delete item controller";
+    @RequestMapping(value = "/item/{id}", method = RequestMethod.DELETE)
+    public String DeleteItemResponce(@PathVariable String id, Model model) {
+        model.addAttribute("id", id);
+        return "Delete item controller. " + id.toString();
     }
 
 }
