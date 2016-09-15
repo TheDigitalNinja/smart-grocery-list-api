7962d73d01000a7ebbc157ff044ea42490adea9e
Russell Perkins <dsw.ninjaboy@gmail.com>
2016-09-15 12:13:28 -0600

    Wired up database connection. closes #8

---
+++ src/main/java/controllers/Item.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    public String getFirstName() {
+        return name;
+    }
+
+    public void setFirstName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/controllers/ItemRepository.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "item", path = "item")
+public interface ItemRepository extends PagingAndSortingRepository<Item, Long> {
+
+    List<Item> findByName(@Param("name") String name);
+
+}
