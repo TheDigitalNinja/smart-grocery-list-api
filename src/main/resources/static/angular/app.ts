471db8df899f85c68bfba897d90d9c464610d1e0
Russell Perkins <dsw.ninjaboy@gmail.com>
2016-09-16 15:44:15 -0600

    Now with angular 2

---
+++ src/main/resources/static/angular/app.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  // Declare the tag name in index.html to where the component attaches
+  selector: 'app',
+
+  // Location of the template for this component
+  templateUrl: 'angular/main.html'
+})
+export class App {
+}
+
