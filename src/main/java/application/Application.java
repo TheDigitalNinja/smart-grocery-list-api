927067be9b88ac520daec78436046ccfaeff2e06
Russell Perkins <dsw.ninjaboy@gmail.com>
2016-09-15 19:54:46 -0600

    cleaned up classes

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,13 +1,13 @@
-package controllers;
+package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationController {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationController.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/application/Item.java
+++ src/main/java/application/Item.java
@@ -1,4 +1,4 @@
-package controllers;
+package application;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
