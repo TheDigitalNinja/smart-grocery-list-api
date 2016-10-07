c59c0e0d428019feccd6286ec90a2b38037edd03
Russell Perkins <dsw.ninjaboy@gmail.com>
2016-10-06 21:07:33 -0600

    cors support

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -3,8 +3,19 @@ package application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public FilterRegistrationBean commonsRequestLoggingFilter()
+    {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new CORSFilter());
+        return registrationBean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
